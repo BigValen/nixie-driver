504c20fbd1e4f06d232696d3901e7af4268756a2
John Looney <john.looney@gmail.com>
2017-05-14 20:52:19 +0100

    First checkin of working code

---
+++ nixie-driver.ino
@@ -0,0 +1,96 @@
+#define CHECK_BIT(var,pos) ((var) & (1<<(pos)))
+
+
+//Pin connected to ST_CP of 74HC595
+int latchPin = 8;
+//Pin connected to SH_CP of 74HC595
+int clockPin = 12;
+////Pin connected to DS of 74HC595
+int dataPin = 11;
+void setup() {
+  //set pins to output because they are addressed in the main loop
+  pinMode(latchPin, OUTPUT);
+  pinMode(dataPin, OUTPUT);
+  pinMode(clockPin, OUTPUT);
+  Serial.begin(115200);
+  Serial.println("reset");
+}
+
+void loop() {
+  if (Serial.available() > 0) {
+    // ASCII '0' through '9' characters are
+    // represented by the values 48 through 57.
+    // so if the user types a number from 0 through 9 in ASCII,
+    // you can subtract 48 to get the actual value:
+    int i = 0;
+
+    digitalWrite(latchPin, LOW);
+
+    String digits = Serial.readStringUntil('\n');
+    for (int i = 0; i < digits.length() ; i=i+2 ) {
+      byte first_number = digits.charAt(i) - 48;
+      byte second_number = digits.charAt(i+1) - 48;
+      byte two_numbers=second_number & 0b00001111 | first_number<<4;
+      // write to the shift register with the correct bit set high:
+      registerWrite(two_numbers, HIGH);
+    }
+  }
+
+  digitalWrite(latchPin, HIGH);
+
+  delay(100);
+}
+
+// This method sends bits to the shift register:
+
+void registerWrite(byte number, int pinState) {
+  shiftOut(dataPin, clockPin, number);
+}
+
+
+
+void shiftOut(int myDataPin, int myClockPin, byte myDataOut) {
+  // This shifts 8 bits out MSB first, 
+  //on the rising edge of the clock,
+  //clock idles low
+
+  //internal function setup
+  int i=0;
+  int pinState;
+  pinMode(myClockPin, OUTPUT);
+  pinMode(myDataPin, OUTPUT);
+
+  //clear everything out just in case to
+  //prepare shift register for bit shifting
+  digitalWrite(myDataPin, 0);
+  digitalWrite(myClockPin, 0);
+
+  //for each bit in the byte myDataOut�
+  //NOTICE THAT WE ARE COUNTING DOWN in our for loop
+  //This means that %00000001 or "1" will go through such
+  //that it will be pin Q0 that lights. 
+  for (i=7; i>=0; i--)  {
+    digitalWrite(myClockPin, 0);
+
+    //if the value passed to myDataOut and a bitmask result 
+    // true then... so if we are at i=6 and our value is
+    // %11010100 it would the code compares it to %01000000 
+    // and proceeds to set pinState to 1.
+    if ( myDataOut & (1<<i) ) {
+      pinState= 1;
+    }
+    else {  
+      pinState= 0;
+    }
+
+    //Sets the pin to HIGH or LOW depending on pinState
+    digitalWrite(myDataPin, pinState);
+    //register shifts bits on upstroke of clock pin  
+    digitalWrite(myClockPin, 1);
+    //zero the data pin after shift to prevent bleed through
+    digitalWrite(myDataPin, 0);
+  }
+
+  //stop shifting
+  digitalWrite(myClockPin, 0);
+}
